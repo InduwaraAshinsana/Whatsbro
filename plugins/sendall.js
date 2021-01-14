600998f445771c101db8afb9ffc7fdbbe46fa110
agentnova <arjunaj2234@gmail.com>
2021-01-14 15:04:47 +0530

    Create sendall.js

---
+++ plugins/sendall.js
@@ -0,0 +1,59 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Language = require('../language');
+const config = require('../config');
+const {DataTypes} = require('sequelize');
+
+const SendallDB = config.DATABASE.define('s_chats', {
+    chat_list: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+
+async function getSendall(jid = null) {
+    var Wher = {chat_list: jid};
+    var Msg = await SendallDB.findAll({
+        where: Wher
+    });
+
+    if (Msg.length < 1) {
+        return false;
+    } else {
+        return Msg;
+    }
+}
+
+async function setSendall(jid = null) {
+    var Msg = await SendallDB.findAll({
+        where: {
+            chat_list: jid
+        }
+    });
+    if (Msg.length < 1) {
+        return await SendallDB.create({chat_list: jid});
+    } else {
+        return false;
+    }
+}
+
+async function deleteSendall(jid = null) {
+    var Msg = await SendallDB.findAll({
+        where: {
+            chat_list: jid
+        }
+    });
+
+    if (Msg.length < 1) {
+        return false;
+    } else {
+        return await Msg[0].destroy();
+    }
+}
+
+Asena.addCommand({pattern: 'set_sendall (.*)', fromMe: true, desc: "Set chatids for sendall"}, (async (message, match) => {
+    await message.sendMessage("got it");
+    await message.sendMessage(match)
+
+}));
